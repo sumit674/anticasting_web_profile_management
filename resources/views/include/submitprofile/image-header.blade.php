e1953745e3ff033a780a78c05903f3309d4cf892
maheshkrsingh <maheshkrs799@gmail.com>
2023-05-07 21:15:21 +0530

    fixed desing issues

--- resources/views/include/submitprofile/image-header.blade.php
+++ resources/views/include/submitprofile/image-header.blade.php
@@ -1,6 +1,9 @@
 <style>
+    .breadcrumbs{
+        background: url('{{ asset('assets/images/banner-left.gif') }}')
+    }
     .welcome-user {
-        background: url('./../../public/assets/images/banner-left.gif')
+
         padding: 5px !important;
         font-size: 22px;
     }
@@ -12,7 +15,7 @@
     <div class="breadcrumbs">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 offset-lg-2 col-12">
+                <div class="col-lg-8 offset-lg-2 col-12 bg-img">
                     <div class="breadcrumbs-content mb-3">
                         <h1 class="page-title">Talent Submission Form</h1>
                     </div>
@@ -20,6 +23,7 @@
                     <ul class="breadcrumb-nav">
                         @auth
                             <li>
+
                                 <span class="welcome-user">Welcome! <b
                                         style="font-size:10px;display: inline-block;"></b>&nbsp;{{ auth()->user()->first_name . ' ' . auth()->user()->last_name }}
                                     &nbsp;</span>
